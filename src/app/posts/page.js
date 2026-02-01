e9d553c6c6552305c44e6c3ad380d9a084415199
arronburgess <generalkajgue@hotmail.com>
2026-02-01 18:44:42 +0000

    chore: final sprint 2

--- src/app/posts/page.js
+++
@@ -1,2 +0,0 @@
-//TODO RENDER A LIST OF ALL POSTS (could also render the author who posted them)
-// We also need to insert the userId into the posts table, make sure you have some SQL that READS the userId from the user's table OR use the auth() function from Clerk to get the  userId
